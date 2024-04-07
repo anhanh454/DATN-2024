a5bb62278f066c46a6fbe30ceead7e62b9ce7ec9
phanthilananh <phanlananh454@gmail.com>
2024-04-07 23:47:41 +0700

    Thêm giao diện

---
+++ FrontEnd/ogani/src/app/components/client/footer/footer.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  email: string = 'lananh2k3@gmail.com';
+  usefulLinks: string[] = [
+    'Về chúng tôi',
+    'Bảo mật thông tin mua hàng',
+    'Thông tin giao hàng',
+    'Chính sách bảo mật',
+    'Dịch vụ',
+    'Liên hệ',
+    'Innovation',
+    'Testimonials'
+  ];
+  newsletterTitle: string = 'Tham gia cùng chúng tôi';
+  newsletterDescription: string = 'Thông báo về thông tin khuyến mãi và mặt hàng mới';
+  subscribeButton: string = 'Đăng ký';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
