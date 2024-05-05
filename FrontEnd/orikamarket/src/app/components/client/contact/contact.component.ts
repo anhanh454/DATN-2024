7b726e7af7d004018a12981ab307ec292036a00b
phanthilananh <phanlananh454@gmail.com>
2024-05-05 14:56:24 +0700

    Hoàn thiện 98%

--- FrontEnd/orikamarket/src/app/components/client/contact/contact.component.ts
+++ FrontEnd/orikamarket/src/app/components/client/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { MessageService } from "primeng/api";
 
 
 @Component({
@@ -7,8 +9,41 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+    invalidForm: boolean = false;
+
+    constructor(private messageService: MessageService) { }
+
+    contactForm: any = {
+        fullname: null,
+        email: null,
+        phone: null,
+        message: null,
+    }
 
     ngOnInit(): void {
-        
+
+    }
+
+    sendEmailMessage() {
+        // Kiểm tra trường hợp null
+        if (!this.contactForm.fullname || !this.contactForm.email || !this.contactForm.phone || !this.contactForm.message) {
+            this.invalidForm = true; // Đánh dấu form là không hợp lệ
+            this.showWarn("Vui lòng nhập đầy đủ thông tin!");
+            return; // Dừng hàm nếu form không hợp lệ
+        }
+
+        this.showSuccess("Gửi tin nhắn thành công!");
+    }
+
+    showSuccess(text: string) {
+        this.messageService.add({ severity: 'success', summary: 'Thành công', detail: text });
+    }
+
+    showError(text: string) {
+        this.messageService.add({ severity: 'error', summary: 'Thất bại', detail: text });
+    }
+
+    showWarn(text: string) {
+        this.messageService.add({ severity: 'warn', summary: 'Cảnh báo', detail: text });
     }
 }
--- FrontEnd/orikamarket/src/app/components/client/footer/footer.component.ts
+++ FrontEnd/orikamarket/src/app/components/client/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-  email: string = 'lananh2k3@gmail.com';
+  invalidForm: boolean = false;
+  email: string = 'phanlananh454@gmail.com';
+  clientEmail: string = '';
   usefulLinks: string[] = [
     'Về chúng tôi',
     'Bảo mật thông tin mua hàng',
@@ -19,8 +22,30 @@ export class FooterComponent implements OnInit {
   newsletterDescription: string = 'Thông báo về thông tin khuyến mãi và mặt hàng mới';
   subscribeButton: string = 'Đăng ký';
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
+  }
+
+  sendEmailRegister() {
+    if (!this.clientEmail) {
+      this.invalidForm = true; // Đánh dấu form là không hợp lệ
+      this.showWarn("Vui lòng nhập email!");
+      return; // Dừng hàm nếu form không hợp lệ
+    }
+
+    this.showSuccess("Gửi tin nhắn thành công!");
+  }
+
+  showSuccess(text: string) {
+    this.messageService.add({ severity: 'success', summary: 'Thành công', detail: text });
+  }
+
+  showError(text: string) {
+    this.messageService.add({ severity: 'error', summary: 'Thất bại', detail: text });
+  }
+
+  showWarn(text: string) {
+    this.messageService.add({ severity: 'warn', summary: 'Cảnh báo', detail: text });
   }
 }
